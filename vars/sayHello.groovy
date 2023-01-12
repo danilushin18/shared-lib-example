cee3e4a1ad39c4d305c5c6e9d9cd81cf262179c9
danilushin18 <77248672+danilushin18@users.noreply.github.com>
2023-01-12 11:34:33 +0200

    Add files via upload

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,5 +1,12 @@
 #!/usr/bin/env groovy
 
-def call(String name = 'human') {
-  echo "Hello, ${name}."
+stage('Build'){
+  steps {
+    def release = false
+      if (release) {
+       sh 'mvn clean install'
+    } else {
+       sh 'mvn package'
+    }
+  }
 }
