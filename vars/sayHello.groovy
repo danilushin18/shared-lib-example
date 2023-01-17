6978e08661a6e19d9882575540e7b1bfa9ba2f99
danilushin18 <77248672+danilushin18@users.noreply.github.com>
2023-01-17 14:01:11 +0200

    Add files via upload

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,10 +1,11 @@
 #!/usr/bin/env groovy
 
  def check(release) {
-  release=release.toBoolean()
+ 
     if (release) {
         sh "mvn clean install"
     } else {
         sh "mvn clean package"
     }
+return release
 }
